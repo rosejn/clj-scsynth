e18ef41fce01a84e0b8580bbef731004253877e3
Fabian Aussems <fabianaussems@gmail.com>
2010-02-24 15:47:25 +0100

    added scsynth dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject clj-scsynth "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Supercollider scsynth bindings"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :native-dependencies [[scsynth "3.3.1"]]
+  :dev-dependencies [[native-deps "1.0.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
+                     [leiningen/lein-swank "1.1.0"]])
